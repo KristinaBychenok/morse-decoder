a6512070bbee104f285acb22258866a8afb9575d
KristinaBychenok <kristinabychenokPR@gmail.com>
2021-08-18 19:37:56 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let obj = {
+        '10': '.',
+        '11': '-',
+        '**********': ' '
+    }
+
+    let arr = []
+    let p = 0;
+    for (let i = 0; i < Math.round(expr.length / 10); i++) {
+        let str = expr.substr(p, 10);
+        p += 10;
+
+        for (letter of str) {
+            if (letter == '*') {
+                arr.push(obj[str])
+                break
+            } else if (letter == '1') {
+                let ourNumber = str.slice(str.indexOf(letter))
+                t = 0;
+                let mors = '';
+                for (let j = 0; j < Math.round(ourNumber.length / 2); j++) {
+                    mors += obj[(ourNumber.substr(t, 2))];
+                    t += 2;
+                }
+                arr.push(MORSE_TABLE[mors])
+                break
+            }
+        }
+    }
+
+    return arr.join('')
+
 }
 
 module.exports = {
